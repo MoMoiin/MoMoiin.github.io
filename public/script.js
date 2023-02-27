50285ab985bee5f808ac6359db59cb982dabf091
MoMo <Kubaadamczyk2002@gmail.com>
2023-02-27 17:04:42 +0000

    First test v1 27/02/2023

---
+++ public/script.js
@@ -0,0 +1,59 @@
+
+
+(function($) { "use strict";
+ 
+ 	//Parallax            
+              
+	function scrollBanner() {
+	  $(document).on('scroll', function(){
+      var scrollPos = $(this).scrollTop();
+        $('.parallax-fade-top').css({
+          'top' : (scrollPos/2)+'px',
+          'opacity' : 1-(scrollPos/700)
+        });
+        $('.parallax-00').css({
+          'top' : (scrollPos/-3.5)+'px'
+        });
+        $('.parallax-01').css({
+          'top' : (scrollPos/-2.8)+'px'
+        });
+        $('.parallax-top-shadow').css({
+          'top' : (scrollPos/-2)+'px'
+        });
+      });    
+	  }
+	scrollBanner();	              
+
+	//Page cursors
+
+    document.getElementsByTagName("body")[0].addEventListener("mousemove", function(n) {
+        t.style.left = n.clientX + "px", 
+		t.style.top = n.clientY + "px", 
+		e.style.left = n.clientX + "px", 
+		e.style.top = n.clientY + "px", 
+		i.style.left = n.clientX + "px", 
+		i.style.top = n.clientY + "px"
+    });
+    var t = document.getElementById("cursor"),
+        e = document.getElementById("cursor2"),
+        i = document.getElementById("cursor3");
+    function n(t) {
+        e.classList.add("hover"), i.classList.add("hover")
+    }
+    function s(t) {
+        e.classList.remove("hover"), i.classList.remove("hover")
+    }
+    s();
+    for (var r = document.querySelectorAll(".hover-target"), a = r.length - 1; a >= 0; a--) {
+        o(r[a])
+    }
+    function o(t) {
+        t.addEventListener("mouseover", n), t.addEventListener("mouseout", s)
+    }              
+              
+
+       
+})(jQuery);
+
+
+	
