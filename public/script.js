da51e460e48ad8ea7091e39137f24a5469807457
MoMo <Kubaadamczyk2002@gmail.com>
2023-03-01 15:02:36 +0000

    Update to Webpage 2v 01/03/23

--- public/script.js
+++ public/script.js
@@ -30,4 +30,49 @@
 })(jQuery);
 
 
-	
+var topSec = document.querySelectorAll(".top");
+
+
+$(window).scroll(function(){
+  var topPos = $(topSec).offset().top;
+  var topBot = topPos + $(topSec).outerHeight(true);
+  if ($(window).scrollTop() > topBot){
+    $(".sidebarNav").addClass("sidebarNavIn");
+  }
+  else{
+    $(".sidebarNav").removeClass("sidebarNavIn");
+  }
+});
+
+var sidebarNavButton = document.querySelectorAll(".sidebarNavItem");
+var sidebarNavSection = document.querySelectorAll(".section");
+
+
+
+
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $(".scrollLink").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 50, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
+
